1f14cb6070c74fc8c6cef49531bbd87552827390
ZEKO31 <115711199+ZEKO31@users.noreply.github.com>
2023-10-13 20:35:50 +0300

    V 1.0.0

---
+++ AVR/application.h
@@ -0,0 +1,3 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
---
+++ AVR/main.c
@@ -0,0 +1,24 @@
+#include <avr/io.h>
+#include <util/twi.h>
+#include "avr/interrupt.h"
+#include "util/delay.h"
+#include "Drivers/MCAL/I2C/I2C.h"
+#include "Drivers/MCAL/TIMER0/TIMER0.h"
+#include "Drivers/MCAL/ADC/ADC.h"
+
+#define F_CPU 8000000UL // Set your microcontroller's clock frequency
+
+
+
+
+
+int main() {
+
+	
+	while(1){
+
+	}
+
+	return 0;
+}
+
